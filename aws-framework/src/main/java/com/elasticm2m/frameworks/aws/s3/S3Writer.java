b51b04a488ad18c5a66ef3565fe9dfa9ab2800c2
Stephen E Ingram <singram@openwhere.com>
2015-09-10 12:13:10 -0400

    Add a min batch size property
    Allows a user to configure a min batch size for writes

--- aws-framework/src/main/java/com/elasticm2m/frameworks/aws/s3/S3Writer.java
+++ aws-framework/src/main/java/com/elasticm2m/frameworks/aws/s3/S3Writer.java
@@ -4,6 +4,7 @@ import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.tuple.Tuple;
+import backtype.storm.utils.Utils;
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
@@ -12,13 +13,24 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.elasticm2m.frameworks.common.base.ElasticBaseRichBolt;
-import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 
 import java.io.ByteArrayInputStream;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.StringJoiner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by singram on 8/30/15.
@@ -28,11 +40,13 @@ public class S3Writer extends ElasticBaseRichBolt {
     private String bucketName;
     private AmazonS3 s3Service;
     private String folderName;
-    private String pathKeyProperty;
-    private String[] paths;
-    private String key2Property;
-    private String[] key2Paths;
-    private boolean logTuple = false;
+    protected LinkedBlockingQueue<Tuple> queue;
+    private ExecutorService executorService;
+    protected Integer batchSize = 500;
+    protected Integer minBatchSize = 25;
+    protected Integer sleepTime = 5;
+    protected ObjectMapper objectMapper;
+    private NumberFormat format2, format3, format4;
 
     @Inject
     public void setBucketName(@Named("s3-bucket-name") String bucketName) {
@@ -45,18 +59,78 @@ public class S3Writer extends ElasticBaseRichBolt {
     }
 
     @Inject
-    public void setPathKeyProperty(@Named("path-key-property") String pathKeyProperty) {
-        this.pathKeyProperty = pathKeyProperty;
-    }
-
-    @Inject(optional = true)
-    public void setkey2Property(@Named("path2-property") String key2Property) {
-        this.key2Property = key2Property;
-    }
+    public void setBatchSize(@Named("bolt_s3_batch") Integer b) { this.batchSize = b; }
 
     @Inject
-    public void setLogTupple(@Named("log-tuple") Boolean logTuple) {
-        this.logTuple = logTuple;
+    public void setMinBatchSize(@Named("bolt_s3_min_batch") Integer b) { this.minBatchSize = b; }
+
+    @Inject
+    public void setSleepTime(@Named("bolt_s3_sleep") Integer b) { this.sleepTime = b; }
+
+    protected static NumberFormat makeFormat(int digits) {
+        NumberFormat retval = NumberFormat.getIntegerInstance();
+        retval.setMinimumIntegerDigits( digits );
+        retval.setGroupingUsed(false);
+        return retval;
+    }
+
+    /**
+     * Create a name with format: year_dayOfYear_hourOfDay_minuteOfHour_secondOfMinute_millisOfSecond
+     *
+     * @param dt DateTime to use
+     * @return String
+     */
+    public String createFileKey(DateTime dt) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(folderName).append("_")
+                .append(format4.format(dt.getYear())).append("_")
+                .append(format2.format(dt.getDayOfYear())).append("_")
+                .append(format2.format(dt.getHourOfDay())).append("_")
+                .append(format2.format(dt.getMinuteOfHour())).append("_")
+                .append(format2.format(dt.getSecondOfMinute())).append("_")
+                .append(format3.format(dt.getMillisOfSecond()));
+        return builder.toString();
+    }
+
+    protected Runnable getTask() {
+        return () -> {
+            logger.info("Checking for S3 Records");
+
+            while(!Thread.currentThread().isInterrupted()) {
+                try {
+                    if (queue.size() >= minBatchSize) {
+                        logger.info("Writing Records to S3");
+                        Tuple tuple = queue.poll();
+                        List<Tuple> tupleList = new ArrayList<>();
+                        while (tuple != null && tupleList.size() < batchSize) {
+                            tupleList.add(tuple);
+                            tuple = queue.poll();
+                        }
+
+                        logger.info("Batch Size: " + tupleList.size() + " Queue Size: " + queue.size());
+                        // Make tuples into a Json array
+                        StringJoiner sj = new StringJoiner(", ", "[", "]");
+                        tupleList.forEach(t -> sj.add(t.getValue(1).toString()));
+                        String body = sj.toString();
+                        byte[] data = body.getBytes();
+                        ObjectMetadata md = new ObjectMetadata();
+                        md.setContentLength( data.length );
+                        PutObjectRequest request = new PutObjectRequest(
+                                bucketName,
+                                createFileKey( DateTime.now(DateTimeZone.UTC) ),
+                                new ByteArrayInputStream(data),
+                                md);
+                        s3Service.putObject(request);
+                        tupleList.forEach(collector::ack);
+                    } else {
+                        Utils.sleep(sleepTime * 1000);
+                    }
+               } catch (Exception e) {
+                    logger.error("Unexpected exception ", e);
+                }
+            }
+            logger.warn("Writing thread is exiting");
+        };
     }
 
     @Override
@@ -76,66 +150,41 @@ public class S3Writer extends ElasticBaseRichBolt {
             throw new IllegalStateException("Bucket [" + bucketName + " does not exist");
         }
 
-        // create Json paths for property keys
-        paths = pathKeyProperty.split("\\.");
-        if ( null != key2Property ) key2Paths = key2Property.split("\\.");
-    }
+        queue = new LinkedBlockingQueue<>(10000);
 
-    protected JsonNode pathFor(JsonNode node, String p[], int idex) {
-        if (idex < p.length) {
-            return pathFor(node.path(p[idex]), p, ++idex); // recurse
-        }
-        else {
-            return node;
-        }
-    }
+        objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.INDENT_OUTPUT, false);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
 
-    protected JsonNode pathFor(JsonNode node, String p[]) {
-        return pathFor(node, p, 0);
-    }
+        format2 = makeFormat(2);
+        format3 = makeFormat(3);
+        format4 = makeFormat(4);
 
-    protected String createKey(JsonNode root) {
-        StringBuilder builder = new StringBuilder();
-        if ( null != folderName ) {
-            builder.append(folderName);
-            if ( !folderName.endsWith("/") ) builder.append("/");
-        }
-        String path = pathFor(root, paths).asText();
-        if ( path.endsWith("/") ) path = path.substring(0, path.length() - 1);
-        builder.append(path);
-        if (null != key2Property)
-        {
-            path = pathFor(root, key2Paths).asText();
-            builder.append("/").append( path );
-        }
-        return builder.toString();
+        executorService = Executors.newSingleThreadExecutor();
+        executorService.submit( getTask() );
     }
 
     @Override
-    public void execute(Tuple tuple) {
+    public void execute(Tuple tuple)  {
+        queue.offer(tuple);
+    }
+
+    @Override
+    public void cleanup() {
+        logger.info("S3 Bulk Writer Stopped");
         try {
-            String body = tuple.getString(1);
-            byte[] data = body.getBytes();
-            ObjectMetadata md = new ObjectMetadata();
-            md.setContentLength( data.length );
-            ObjectMapper mapper = new ObjectMapper();
-            PutObjectRequest request = new PutObjectRequest(
-                    bucketName,
-                    createKey( mapper.readValue(body, JsonNode.class) ),
-                    new ByteArrayInputStream(data),
-                    md);
-            s3Service.putObject(request);
-
-            if (logTuple) {
-                logger.info(body);
-            } else {
-                logger.debug("Published record to S3");
+            executorService.shutdown();
+            executorService.awaitTermination(5, TimeUnit.SECONDS);
+        }
+        catch (InterruptedException e) {
+            logger.error("Shutdown interrupted");
+        }
+        finally {
+            if (!executorService.isTerminated()) {
+                logger.info("cancel non-finished tasks");
             }
-
-            collector.ack(tuple);
-        } catch (Throwable ex) {
-            logger.error("Error writing the entity to S3:", ex);
-            collector.fail(tuple);
+            executorService.shutdownNow();
         }
     }
 
